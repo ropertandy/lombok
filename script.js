1e218c1aa35eb24f4e6db448b325b0cfbcf19e80
unknown <justinemalabuyoc1@gmail.com>
2024-04-11 13:03:31 +0800

    about lombok

---
+++ script.js
@@ -0,0 +1,7 @@
+const headerNav = document.querySelector (".header__nav");
+const toggleMenu = document.querySelector (".toggle__menu");
+
+toggleMenu.addEventListener("click", () => {
+    headerNav.classList.toggle("open");
+    toggleMenu.classList.toggle("close");
+});
